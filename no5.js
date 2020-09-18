3ebddd5853123ca347342b95092fab234ae7e3c0
Your Name <you@example.com>
2020-09-18 21:57:55 +0700

    evaluasi

---
+++ no5.js
@@ -0,0 +1,8 @@
+function santriProgrammer(nama,Status,asal) {
+    console.log('nama santri ' + nama, 'berasal dari kota' + asal)
+    status()
+}
+function status() {
+    alert('Status nya adalah santri');
+}
+santriProgrammer('Rahmad',status,'Medan');
